d0c0bdc0c9b16f23679e020a644ae3ad17c84bbd
Vinicius Biondi <viniciusvwxyz@gmail.com>
2018-02-23 16:54:09 -0300

    add Analysis and AnalysisResult models

---
+++ Analysis.py
@@ -0,0 +1,6 @@
+class Analysis:
+    def __init__(self,
+                 id=0,
+                 idImage=0):
+        self.id = id
+        self.idImage = idImage
---
+++ AnalysisResult.py
@@ -0,0 +1,10 @@
+class AnalysisResult:
+    def __init__(self,
+                 id=0,
+                 idAnalysis=0,
+                 idDisease=0,
+                 score=0):
+        self.id = id
+        self.idAnalysis = idAnalysis
+        self.idDisease = idDisease
+        self.score = score
