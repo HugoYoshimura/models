77a633eabe1a9d8b42c907c760a971dc74f49641
HugoYoshimura <hugomasayoshi@gmail.com>
2018-03-18 19:09:15 -0300

    Add Classifier model

---
+++ Classifier.py
@@ -0,0 +1,9 @@
+from models.Plant import Plant
+
+
+class Classifier:
+	def __init__(self,id=0, id_plant=0, tag="", path=""):
+		self.id = id
+		self.id_plant = id_plant
+		self.tag = tag
+		self.path = path
