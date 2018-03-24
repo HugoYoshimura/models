53adf95674ff729b25557e45b279a8a4efcc3651
HugoYoshimura <HugoYoshimura@users.noreply.github.com>
2018-03-24 11:08:04 -0300

    Add Classifier Model (#13)
    * Add Classifier model
    
    * Add Classifier Model
    
    * Remove Classifier.py.swp
    
    * Update Classifier Model
    
    * Fixed Classifier Database
    
    * Fixed Classifier Models
    
    * Fixed Classifier Models

---
+++ Classifier.py
@@ -0,0 +1,18 @@
+from models.Plant import Plant
+
+
+
+
+class Classifier:
+    def __init__(self,
+		 id=0, 
+		 plant=Plant(), 
+		 tag="", 
+		 path="",
+		 analyses=list()):
+
+        self.id = id
+        self.plant = plant
+        self.tag = tag
+        self.path = path
+        self.analyses = analyses
