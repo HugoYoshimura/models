80e0573af63250d94d5e40edb5b38bc9b26037d9
HugoYoshimura <hugomasayoshi@gmail.com>
2018-03-24 10:09:20 -0300

    Fixed Classifier Models

--- Classifier.py
+++ Classifier.py
@@ -4,15 +4,15 @@ from models.Plant import Plant
 
 
 class Classifier:
-	def __init__(self,
+    def __init__(self,
 		     id=0, 
 		     plant=Plant(), 
 		     tag="", 
 		     path="",
 		     analyses=list()):
 
-	self.id = id
-	self.plant = plant
-	self.tag = tag
-	self.path = path
-	self.analyses = analyses
+	    self.id = id
+	    self.plant = plant
+	    self.tag = tag
+	    self.path = path
+	    self.analyses = analyses
