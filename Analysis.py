c71b7b8f0925de685cbf89db0060568e70749991
Vinicius Biondi <viniciusvwxyz@gmail.com>
2018-04-08 19:23:32 -0300

    Fix parameters at Analysis and AnalysisResult (#15)
    * Fix parameters
    
    * fix parameters
    
    * Documentation for Analysis and AnalysisResult classes
    
    * Fix coment in constructor method
    
    * Fix identation

--- Analysis.py
+++ Analysis.py
@@ -2,12 +2,26 @@ from models.Classifier import Classifier
 from models.Image import Image
 
 
-
 class Analysis:
+    """An Analysis object contains information about an executed image analysis.
+
+    Attributes:
+        id: Identification number of this object
+        image: Image object that has been used for this Analysis
+        classifier: Classifier object that has been used for this Analysis
+    """
+
+
     def __init__(self,
                  id=0,
-                 image=object(),
-                 classifier=object()):
+                 image=Image(),
+                 classifier=Classifier():
+        """Analisys model constructor
+        Args:
+            id: Integer number to identification
+            image: Image object that has been used for this Analysis
+            classifier: Classifier object that has been used for this Analysis
+        """
         self.id = id
         self.image = image
         self.classifier = classifier
--- AnalysisResult.py
+++ AnalysisResult.py
@@ -1,10 +1,32 @@
+from models.Disease import Disease
+from models.Analysis import Analysis
+
+
 class AnalysisResult:
+    """An AnalysisResult object contains the result of an executed analysis.
+
+    Attributes:
+        id    Identification number of this object
+        analysis    Analysis object that gave this result
+        disease    Disease object that was predicted by the Analysis classifier
+        score    Score of result certainty
+    """
+
+
     def __init__(self,
                  id=0,
-                 idAnalysis=0,
-                 idDisease=0,
+                 analysis=Analysis(),
+                 disease=Disease(),
                  score=0):
+        """AnalisysResult model constructor
+
+        args:
+            id: Integer ID number
+            analysis: Analysis object that gave this result
+            disease: Disease object that was predicted by the Analysis classifier
+            score: Score of result certainty
+        """
         self.id = id
-        self.idAnalysis = idAnalysis
-        self.idDisease = idDisease
+        self.analysis = analysis
+        self.disease = disease
         self.score = score
