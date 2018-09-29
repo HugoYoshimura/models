0383a413a11919a7c837ac8199f65a567c5e1bea
Wendel Hime <wendelhime@hotmail.com>
2018-09-29 11:49:10 -0300

    add relationship between analysis and user

--- Analysis.py
+++ Analysis.py
@@ -1,5 +1,6 @@
 from models.Classifier import Classifier
 from models.Image import Image
+from models.User import User
 
 
 class Analysis:
@@ -10,22 +11,26 @@ class Analysis:
         image: Image object that has been used for this Analysis
         classifier: Classifier object that has been used for this Analysis
         analysis_results: A list of AnalysisResult object with all results
+        user: User which perform this analysis
     """
 
-
-    def __init__(self,
-                 id=0,
-                 image=Image(),
-                 classifier=Classifier(),
-                 analysis_results=[]):
+    def __init__(
+            self,
+            id=0,
+            image=Image(),
+            classifier=Classifier(),
+            analysis_results=[],
+            user=User()):
         """Analisys model constructor
         Args:
             id: Integer number to identification
             image: Image object that has been used for this Analysis
             classifier: Classifier object that has been used for this Analysis
             analysis_results: A list of AnalysisResult object with all results
+            user: User which perform this analysis
         """
         self.id = id
         self.image = image
         self.classifier = classifier
         self.analysis_results = analysis_results
+        self.user = user
